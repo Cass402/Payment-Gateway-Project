24a9a5c1dcd2cdad35edfd35e9fb54b4cabac489
Srivatsav Erramilli <srivatsaverramilli@gmail.com>
2023-11-14 21:23:44 -0700

    Environmental variables have been used. PostgreSQL database has been created. Changes to the README.md file have been made.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Payment-Gateway-Project
-An individual simulating a payment gateway system
+
+This is an individual project that simulates a payment gateway to demonstrate expertise in related concepts.
+
+The project uses node.js packages and uses express, pg and dotenv.
+All dependencies are in the pacakge.json file with their versions.
+Environment variables are used to promote security and removing any sensitive hardcoded data.
+Server.js is the application start point.
+PostgreSQL is installed and the database has been created. The tables have to be made.
--- server.js
+++ server.js
@@ -4,19 +4,23 @@
 // Importing the express module
 const express = require("express");
 const { Pool } = require("pg"); // Importing the node-postgres module
+require("dotenv").config(); // Importing the dotenv module for local environment variables
 
 const app = express(); // Creating an express application
 const pool = new Pool({
   // Creating a new pool object
-  user: "dbuser", // The user name for the database
-  host: "localhost", // The host where the database server is running
-  database: "my_database", // The database name
-  password: "secret", // The password for the database user
-  port: 5432, // The port where the database server is listening
+  user: process.env.DB_USER, // The user name for the database
+  host: process.env.DB_HOST, // The host where the database server is running
+  database: process.env.DB_DATABASE, // The database name
+  password: process.env.DB_PASSWORD, // The password for the database user
+  port: process.env.DB_PORT, // The port where the database server is listening
   idleTimeoutMillis: 30000, // The time to wait before terminating the connection
+  //TODO - Add SSL
 });
 
 app.use(express.json()); // Adding the express.json middleware
+
+//TODO - Add routes
 
 app.get("/", (eq, res) => {
   // Defining the handler function for the / route
